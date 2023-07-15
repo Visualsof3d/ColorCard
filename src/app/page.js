1de2fe14d70337558258bc21cc959540a46dde3b
Visualsof3d <rajanrk5151@gmail.com>
2023-07-15 15:29:08 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,69 @@
-import Image from 'next/image'
+'use client';
+import { gsap } from 'gsap'
 import styles from './page.module.css'
 
+
+const projects = [
+  {
+    title: "New York",
+    color: "#F06318"
+  },
+  {
+    title: "San Francisco",
+    color: "#DCF018"
+  },
+  {
+    title: "San Antonio",
+    color: "#18F0E8"
+  },
+  {
+    title: "Nashville",
+    color: "#8C0CF0"
+  },
+  {
+    title: "Houston",
+    color: "#F0183C"
+  },
+  {
+    title: "New Orleans",
+    color: "#F0BA18"
+  },
+  {
+    title: "Oklahoma City",
+    color: "#0C34F0"
+  },
+  {
+    title: "Detroit",
+    color: "#0CBCF0"
+  },
+  {
+    title: "Indianapolis",
+    color: "#91F018"
+  }
+]
+
+
 export default function Home() {
+  
+  const manageMouseEnter = (expand, color) => {
+    gsap.to(expand.target, {top: "-2vw", backgroundColor: color, duration: 0.3})
+  }
+  const manageMouseLeave = (expand, color) => {
+    gsap.to(expand.target, {top: "0vw", backgroundColor: "white", duration: 0.3, delay: 0.1})
+  }
+  
+  
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      {
+        projects.map((project, index) => {
+          return <div onMouseEnter={(expand) => {manageMouseEnter(expand, project.color)}} onMouseLeave={(expand) => {manageMouseLeave(expand, project.color)}} key={index}>
+            <p>
+              {project.title}
+            </p>
+          </div>
+        })
+      }
     </main>
   )
 }
